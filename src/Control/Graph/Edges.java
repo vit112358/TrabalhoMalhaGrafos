d809acc47fc2ed904c54ccf1da220cd484ef4c4b
vitor <vitorff1234@gmail.com>
2018-10-15 19:58:14 -0300

    Correções Sonar - Leitura Início Modelagem

---
+++ src/Control/Graph/Edges.java
@@ -0,0 +1,64 @@
+package Control.Graph;
+
+import java.util.Objects;
+
+public class Edges {
+    private int id;
+    private Vertex origem;
+    private Vertex destino;
+
+    public Edges(int id, Vertex origem, Vertex destino) {
+        this.id = id;
+        this.origem = origem;
+        this.destino = destino;
+    }
+
+    public Edges() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Vertex getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(Vertex origem) {
+        this.origem = origem;
+    }
+
+    public Vertex getDestino() {
+        return destino;
+    }
+
+    public void setDestino(Vertex destino) {
+        this.destino = destino;
+    }
+
+    @Override
+    public String toString() {
+        return "Edges{" +
+                "id=" + id +
+                ", origem=" + origem +
+                ", destino=" + destino +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Edges edges = (Edges) o;
+        return id == edges.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- src/Control/IO/Leitura.java
+++ src/Control/IO/Leitura.java
@@ -10,32 +10,27 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Leitura {
 
-    public List<t> readFile(File Caminho){
+    public List<t> readFile(File caminho){
         List<t> ts = new ArrayList<>();
         try {
-            try (BufferedReader meu_buffer = new BufferedReader(new FileReader(Caminho))) {
+            try (BufferedReader meuBuffer = new BufferedReader(new FileReader(caminho))) {
                 String linha;
-                try {
-                    //enquanto eu conseguir ler o arquivo faço algo
-                    while (meu_buffer.ready()) {
+                //enquanto eu conseguir ler o arquivo faço algo
+                while (meuBuffer.ready()) {
 
-                        //lendo a linha
-                        linha = meu_buffer.readLine();
+                    //lendo a linha
+                    linha = meuBuffer.readLine();
 
-                        //quebrando a linha
-                        String[] valores = linha.split(" ");
-                        t aux = new t(Integer.parseInt(valores[0]),Integer.parseInt(valores[1]), Integer.parseInt(valores[2]));
-                        ts.add(aux);
+                    //quebrando a linha
+                    String[] valores = linha.split(" ");
+                    t aux = new t(Integer.parseInt(valores[0]),Integer.parseInt(valores[1]), Integer.parseInt(valores[2]));
+                    ts.add(aux);
 
-                    }
-                } catch (IOException ex) {
-                    Logger.getLogger(Leitura.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
         } catch (FileNotFoundException ex) {
