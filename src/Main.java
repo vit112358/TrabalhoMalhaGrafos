f5befa782f8af39bb8e73211284d40674acb8ab1
vitor <vitorff1234@gmail.com>
2018-11-13 03:19:42 -0200

    Menu

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ import Model.AuxStruct.Voo;
 import Model.Estrutura;
 import Model.GraphRota.Vertex;
 
+import javax.swing.JOptionPane;
 import java.io.File;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -20,33 +21,9 @@ public class Main {
     public static void main(String[] args) {
 
         Leitura l = new Leitura();
-
         File f = new File("C:\\Users\\vitor\\Documents\\NetBeansProjects\\TrabalhoED2\\voos.txt");
-        
         Estrutura e = l.readFile(f);
         Controls c = new Controls();
-        List<Voo> v=c.buscaVoosDiretos(e);
-
-        List<Aeroporto> aero = new ArrayList<>();
-
-        for (Map.Entry<String, Vertex> aeroporo: e.getRotas().getVertices().entrySet()) {
-            RotaManager.addAirport(aeroporo.getValue().getAeroporto());
-        }
-
-        Populacao p = new Populacao(50, true, e);
-
-        System.out.println("Distância inicial: " + (int)p.getFittest().getDistance());
-
-        //Evoluí a população por 200 gerações
-        for (int i = 0; i < 200; i++) {
-            p = GeneticAlgorithm.evolvePopulation(p);
-        }
-
-        //Imprime o comprimento da melhor rota da população final
-        System.out.println("Distância final: " + (int)p.getFittest().getDistance());
-        //Imprime a solução encontrada para o problema
-        System.out.println("Solução:");
-        System.out.println(p.getFittest());
 
         System.out.println("==============Programa de Demonstração de Rotas Aéreas============================");
         System.out.println("\n");
@@ -59,23 +36,48 @@ public class Main {
         System.out.println("==============3 -> Viagem de Menor Custo entre 2 destinos ========================");
         System.out.println("==============4 -> Cobertura de Aeroportos =======================================");
         System.out.println("==============5 -> Tempo Minímo ==================================================");
-
+        System.out.println("==============0 -> Sair ==========================================================");
         int op=-1;
+        while(op!=0){
+            op = Integer.parseInt(JOptionPane.showInputDialog("Digite o Código"));
+            switch (op){
+                case 11:
+                    break;
+                case 12:
+                    List<Voo> v=c.buscaVoosDiretos(e);
+                    for (Voo voo:v) {
+                        System.out.println(voo.toString());
+                    }
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    for (Map.Entry<String, Vertex> aeroporo: e.getRotas().getVertices().entrySet()) {
+                        RotaManager.addAirport(aeroporo.getValue().getAeroporto());
+                    }
 
-        switch (op){
-            case 11:
-                break;
-            case 12:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
+                    Populacao p = new Populacao(50, true, e);
+
+                    System.out.println("Distância inicial: " + (int)p.getFittest().getDistance());
+
+                    //Evoluí a população por 200 gerações
+                    for (int i = 0; i < 200; i++) {
+                        p = GeneticAlgorithm.evolvePopulation(p);
+                    }
+
+                    //Imprime o comprimento da melhor rota da população final
+                    System.out.println("Distância final: " + (int)p.getFittest().getDistance());
+                    //Imprime a solução encontrada para o problema
+                    System.out.println("Solução:");
+                    System.out.println(p.getFittest());
+                    break;
+                case 0:
+                    break;
+            }
         }
-
     }
 }
